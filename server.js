7f3a0c527d0c873436385509e3f55538a6eed2d4
Osniel Valdivia <osnielvaldivia@gmail.com>
2019-09-30 17:00:14 -0400

    WIP on automated http tests

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const jsYaml = require('js-yaml');
+const fs = require('fs');
+const { OpenApiValidator } = require('express-openapi-validate');
 const app = express();
+
+// OpenAPI functionality
+const openApiDocument 
+    = jsYaml.safeLoad(fs.readFileSync('./spec/api.yaml', 'utf-8'));
+
+const validator = new OpenApiValidator({apiSpec: './spec/api.yaml'}).install(app);
 
 // Connect Database;
 connectDB();
@@ -15,6 +24,7 @@ app.use(
 app.get('/', (req, res) => res.send('API Running'));
 
 // Define Routes
+app.use('/api/spec', express.static(spec));
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/company', require('./routes/api/company'));
@@ -22,4 +32,6 @@ app.use('/api/employee', require('./routes/api/employee'));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
+module.exports = server;
